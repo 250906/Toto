d340a18289acb88ac859ad6892c87cccd1af241c
250906 <122768193+250906@users.noreply.github.com>
2023-10-23 15:17:23 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById("btn_1").onclick = function () {
+    alert("1반에 몰빵하셨읍니다.");
+}
+
+document.getElementById("btn_2").onclick = function () {
+    alert("2반에 몰빵하셨읍니다.");
+}
+
+document.getElementById("btn_3").onclick = function () {
+    alert("3반에 몰빵하셨읍니다.");
+}
+
+document.getElementById("btn_4").onclick = function () {
+    alert("4반에 몰빵하셨읍니다.");
+}
+
+document.getElementById("btn_5").onclick = function () {
+    alert("5반에 몰빵하셨읍니다.");
+}
+
+document.getElementById("btn_6").onclick = function () {
+    alert("6반에 몰빵하셨읍니다.");
+}
+
+document.getElementById("btn_7").onclick = function () {
+    alert("7반에 몰빵하셨읍니다.");
+}
+
+document.getElementById("btn_8").onclick = function () {
+    alert("8반에 몰빵하셨읍니다.");
+}
